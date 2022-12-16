45d99c7cb2972672b0e12e6479fed10f8badd389
Michael Elfassy <mike.elfassy@gmail.com>
2022-12-15 19:49:13 -0500

    commit tree ecac1a86cafcf50795b084f94535a3b393b99898

--- main.js
+++ main.js
@@ -3,8 +3,57 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let errorModal = document.querySelector('#modal')
 
 
+// Add click event listeners
+document.querySelectorAll('article').forEach( post => {
+  let postId = post.id
+  let heartGlyph = post.querySelector('.like-glyph')
+  heartGlyph.addEventListener('click', heartClick => handleHeartClick(heartClick, postId))
+})
+
+function handleHeartClick (heartClick, postId) {
+  let currentHeart = heartClick.target
+  
+  console.log('Post ID:', postId)
+  console.log('Current Heart:', currentHeart.textContent)
+
+  mimicServerCall() // PATCH with postId and currentHeart
+    .then((data) => {
+      console.log('200', data)
+      console.log('New Heart:', flipHeart(currentHeart))
+    })
+    .catch((data) => {
+      console.log('400', data)
+      displayError(data)
+    })
+
+}
+
+
+// Helper Functions
+
+function displayError(errorMessage) {
+  errorModal.className = ''
+  errorModal.querySelector('#modal-message').textContent = errorMessage
+  setTimeout(() => errorModal.className = 'hidden', 3000)
+}
+
+function flipHeart(currentHeart) {
+  // Like
+  if (currentHeart.textContent === EMPTY_HEART) {
+    currentHeart.textContent = FULL_HEART
+    currentHeart.className = 'like-glyph activated-heart'
+    return FULL_HEART
+  }
+  // Unlike
+  else {
+    currentHeart.textContent = EMPTY_HEART
+    currentHeart.className = 'like-glyph'  
+    return EMPTY_HEART
+  }
+}
 
 
 //------------------------------------------------------------------------------
